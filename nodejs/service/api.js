e9c4f64f1508b782d035534ce86ff2c0c70c7035
sakda homhuan <sakda.homhuan@gmail.com>
2023-03-31 10:32:06 +0700

    update

--- nodejs/service/api.js
+++ nodejs/service/api.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+// fetch data
+const getHotspot = () => {
+    const url = "https://firms.modaps.eosdis.nasa.gov/mapserver/wfs/SouthEast_Asia/c56f7d70bc06160e3c443a592fd9c87e/?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAME=ms:fires_snpp_24hrs&STARTINDEX=0&COUNT=10000&SRSNAME=urn:ogc:def:crs:EPSG::4326&BBOX=-90,-180,90,180,urn:ogc:def:crs:EPSG::4326&outputformat=geojson"
+
+}
+
+
 router.get('/api', (req, res) => {
     res.send('Hello World!');
 });
